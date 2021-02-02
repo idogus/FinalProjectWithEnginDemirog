e1ca87cfd37cb7d82a325512896dcc0b39c40e35
İbrahim Doğuş <idogus@gmail.com>
2021-02-02 08:59:01 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productService.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
