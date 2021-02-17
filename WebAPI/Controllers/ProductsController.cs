87b07c013a66e821ef5067be54ef3c1e4187430b
İbrahim Doğuş <idogus@gmail.com>
2021-02-18 00:17:28 +0300

    Added ValidationAspect

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -17,19 +17,42 @@ namespace WebAPI.Controllers
     {
         //loosely coupled
         //naming convention
+        //IoC Container -- Inversion of Control
         IProductService _productService;
         public ProductsController(IProductService productService)
         {
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult Get()
         {
+            //Swagger
             //Dependency chain - Bağımlılık zinciri
-            
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
