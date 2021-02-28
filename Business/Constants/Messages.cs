759b4879c0e5901d5bc349cf6e191436e17097a1
İbrahim Doğuş <idogus@gmail.com>
2021-02-28 09:26:27 +0300

    Added Authorization Aspects

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +14,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "ilgili kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde ürün öncden mevcut!";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkiniz bulunmuyor.";
+        public static string UserRegistered = "Kayıt olundu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatalı";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten var.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
     }
 }
